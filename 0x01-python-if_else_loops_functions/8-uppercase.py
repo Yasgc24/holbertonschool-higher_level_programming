d2f6d89988cb71ecacf40a36c237b6c6a6216e8f
Yasgc24 <4537@holbertonschool.com>
2022-05-03 16:04:53 -0700

    Add task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,7 @@
 def uppercase(str):
     for char in str:
         letter = ord(char)
-        if letter > 96 and letter < 124:
-            letter -= letter
+        if letter > 96 and letter < 123:
+            letter -= 32
         print(char(letter), end='')
     print()
-
