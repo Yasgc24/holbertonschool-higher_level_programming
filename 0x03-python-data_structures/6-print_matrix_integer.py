11f68e35987e0219aaf51d06b3083ec903fb5969
Yasgc24 <4537@holbertonschool.com>
2022-05-09 12:32:59 -0700

    Add task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for x in matrix:
+        for i in range(len(x)):
+            print((x[i]), end="")
+            if i != len(x) - 1:
+                print(" ", end="")
+        print("")
