0087017220823b9b28329e52443c0c14531e9acc
Yasgc24 <4537@holbertonschool.com>
2022-05-09 14:06:50 -0700

    Correction

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if (len(sentence)) == 0:
+    sentencel = len(sentence)
+    if sentencel == 0:
         firstrchar = None
     else:
         firstchar = sentence[0]
-    return ((len(sentence)), firstchar)
+    return (sentencel, firstchar)
