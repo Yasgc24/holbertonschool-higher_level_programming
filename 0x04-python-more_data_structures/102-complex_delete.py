099d58ebf49a427da665ca122fff45b465d00cfd
Yasgc24 <4537@holbertonschool.com>
2022-05-10 15:31:52 -0700

    Add task 14 correction

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,7 +2,5 @@
 def complex_delete(a_dictionary, value):
     for key in list(a_dictionary.keys()):
         if a_dictionary[key] == value:
-           a_dictionary.pop(key)
+            a_dictionary.pop(key)
     return a_dictionary
-
-
