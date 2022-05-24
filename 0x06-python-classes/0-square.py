a6d78528fa93f44476dade2e8a52d94b391d5804
Yasgc24 <4537@holbertonschool.com>
2022-05-24 10:01:16 -0700

    Add task 0 and 1 solutions

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""0. My first square"""
+
+
+class Square:
+    """empty class Square that defines a square"""
+
+
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""1. Square with size"""
+
+
+class Square:
+    """class Square that defines a square
+    Private instance attribute: size
+    Instantiation with size"""
+
+
+    def __init__(self, size):
+        self.__size = size
