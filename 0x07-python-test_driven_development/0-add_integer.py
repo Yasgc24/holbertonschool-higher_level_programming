27f624fa3c963dc73b85da643efb411a8ee5b23a
Yasgc24 <4537@holbertonschool.com>
2022-05-26 10:04:10 -0700

    Add files empty and solution of the task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+0. Integers addition
+"""
+
+
+def add_integer(a, b=98):
+    """
+    function that adds 2 integers
+    """
+    if not type(a) == int and not type(a) == float:
+        """
+        raise a TypeError exception if a is not interger or float
+        """
+        raise TypeError("a must be an integer")
+    if not type(b) == int and not type(b) == float:
+        """
+        raise a TypeError exception if b is not interger or float
+        """
+        raise TypeError("b must be an integer")
+
+    if type(a) == float:
+        a = int(a)
+    if type(b) == float:
+        b = int(b)
+
+    return (a + b)
