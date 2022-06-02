a88d1f1a4803d5c8f3395431c78cb0253a38d3e9
Yasgc24 <4537@holbertonschool.com>
2022-06-02 17:40:20 -0500

    tests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -9,33 +9,32 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-        """
-        Test cases
-        """
+        """Test cases"""
+        
         def max_at_end(self):
-                list = [10, 20, 30, 40]
-                self.assertEqual(max_integer(list),40)
+            list = [10, 20, 30, 40]
+            self.assertEqual(max_integer(list),40)
 
         def max_at_beggining(self):
-                list = [40, 30, 20, 10]
-                self.assertEqual(max_integer(list), 40)
+            list = [40, 30, 20, 10]
+            self.assertEqual(max_integer(list), 40)
     
         def max_in_middle(self):
-                list = [5, 10, 15, 12, 8, 4]
-                self.assertEqual(max_integer(list), 15)
+            list = [5, 10, 15, 12, 8, 4]
+            self.assertEqual(max_integer(list), 15)
 
         def one_negative(self):
-                list = [0, -20, 40, 60]
-                self.assertEqual(max_integer(list), 60)
+            list = [0, -20, 40, 60]
+            self.assertEqual(max_integer(list), 60)
 
         def negative_numbers(self):
-                list = [-2, -4, -6, -8]
-                self.assertEqual(max_integer(list), -2)
+            list = [-2, -4, -6, -8]
+            self.assertEqual(max_integer(list), -2)
 
         def one_element(self):
-                list = [5]
-                self.assertEqual(max_integer(list), 5)
+            list = [5]
+            self.assertEqual(max_integer(list), 5)
 
         def list_empty(self):
-                list = []
-                self.assertEqual(max_integer(list), None)
+            list = []
+            self.assertEqual(max_integer(list), None)
