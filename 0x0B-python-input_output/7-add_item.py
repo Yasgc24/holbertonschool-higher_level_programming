b133c1cc9a1b226f45e21b39fae64be6569f4b14
Yasgc24 <4537@holbertonschool.com>
2022-06-08 18:54:06 -0500

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """7. Load, add, save"""
-import json
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-args = sys.argv
-filename = "add_item.json"
-with open(filename, "a", encoding="utf-8") as f:
-    my_list = []
-    my_list.extend(args[1:])
-    save_to_json_file(my_list, filename)
-    load_from_json_file(filename)
+try:
+    item = load_from_json_file("add_item.json")
+except Exception:
+    item = []
+for a in range(1, len(sys.argv)):
+    item.append(sys.argv[a])
+save_to_json_file(item, "add_item.json")
