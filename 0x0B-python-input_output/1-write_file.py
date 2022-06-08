564ed98eca6d35708ec362a63fefa255e436c39f
Yasgc24 <4537@holbertonschool.com>
2022-06-08 16:52:29 -0500

    pycodestyle

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,7 +3,8 @@
 
 
 def write_file(filename="", text=""):
-    """ writes a string to a text file (UTF8) and returns the number of characters written"""
-    with open(filename, "w", encoding = "utf-8") as f:
+    """ writes a string to a text file (UTF8)
+    and returns the number of characters written"""
+    with open(filename, "w", encoding="utf-8") as f:
         write_data = f.write(text)
         return (write_data)
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,6 +5,6 @@
 def append_write(filename="", text=""):
     """appends a string at the end of a text file
     (UTF8) and returns the number of characters added"""
-    with open(filename, "a", encoding = "utf-8") as f:
+    with open(filename, "a", encoding="utf-8") as f:
         write_data = f.write(text)
         return (write_data)
