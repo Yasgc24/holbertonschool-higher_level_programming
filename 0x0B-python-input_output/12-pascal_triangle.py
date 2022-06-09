4d3253e92b9d79beced707fb535b8a2be66667bb
Yasgc24 <4537@holbertonschool.com>
2022-06-08 19:10:00 -0500

    task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -2,3 +2,18 @@
 """12. Pascal's Triangle"""
 
 
+def pascal_triangle(n):
+    """returns a list of lists of integers
+    representing the Pascal’s triangle of n"""
+    if n <= 0:
+        return []
+
+    pascal_trian = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        for p in range (1, i):
+            row.append(pascal_trian[i-1][p-1] + pascal_trian[i-1][p])
+        row.append(1)
+        pascal_trian.append(row)
+    return pascal_trian
