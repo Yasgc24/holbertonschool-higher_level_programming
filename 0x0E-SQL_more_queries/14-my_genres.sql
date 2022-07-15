a88edb3956d4e6165f1a04f96bbf7e99591fad7d
Yasgc24 <4537@holbertonschool.com>
2022-07-14 22:31:16 -0500

    Add task 14 solution

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,3 +2,8 @@
 -- The tv_shows table contains only one record where title = Dexter (but the id can be different)
 -- Each record should display: tv_genres.name
 -- Results must be sorted in ascending order by the genre name
+SELECT tv_genres.name
+FROM tv_genres
+JOIN tv_show_genres ON tv_show_genres.genre_id=tv_genres.id
+JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
+ORDER BY tv_genres.name ASC;
