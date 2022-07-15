f497c57e6e519b975877bbb2bfc762b8ec11e17b
Yasgc24 <4537@holbertonschool.com>
2022-07-14 22:43:23 -0500

    modified task 15

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -2,9 +2,9 @@
 -- The tv_genres table contains only one record where name = Comedy (but the id can be different)
 -- Each record should display: tv_shows.title
 -- Results must be sorted in ascending order by the show title
-SELECT tv_genres.name
-FROM tv_genres
-JOIN tv_show_genres ON tv_show_genres.genre_id=tv_genres.id
-JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
-WHERE tv_shows.title='Dexter'
-ORDER BY tv_genres.name ASC;
+SELECT tv_shows.title
+FROM tv_shows
+JOIN tv_show_genres ON tv_show_genres.show_id=tv_shows.id
+JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
+WHERE tv_genres.title='Comedy'
+ORDER BY tv_shows.title ASC;
