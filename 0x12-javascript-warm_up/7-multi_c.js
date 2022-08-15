0db72b86bb9465751b17931fabbe430b73656d7e
Yasgc24 <4537@holbertonschool.com>
2022-08-15 18:27:25 -0500

    Add script task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const args = process.argv;
+const times = parseInt(args[2]);
+if (times) {
+  for (let i = 0; i < times; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
