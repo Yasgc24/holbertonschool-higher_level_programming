ae4bf1690a015995ac987a1ddf9764c1cb14afe8
Yasgc24 <4537@holbertonschool.com>
2022-08-15 20:04:38 -0500

    Add script task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+const args = process.argv;
+const num = parseInt(args[2]);
+function factorial (num) {
+  if (num === 0) {
+    return 1;
+  }
+  if (!num) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
+console.log(factorial(num));
