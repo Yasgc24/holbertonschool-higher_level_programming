e3c5dce0cd08e25211dcfd7193f0fc2673fdd4bd
Yasgc24 <4537@holbertonschool.com>
2022-08-17 16:17:38 -0500

    Add task 3 solution

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
+const URL = 'https://swapi-api.hbtn.io/api/films/:id' + process.argv[2];
 
-request.get('https://swapi-api.hbtn.io/api/films/:id' + process.argv[2], (error, response, body) => {
+request.get(URL, (error, response, body) => {
   if (error) {
     console.error('error:', error);
   }
