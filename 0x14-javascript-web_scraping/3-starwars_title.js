2292edcc26d16b5546e357a52e332e6ab99241b8
Yasgc24 <4537@holbertonschool.com>
2022-08-17 16:16:11 -0500

    Add task 3 solution

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const request = require('request');
+
+request.get('https://swapi-api.hbtn.io/api/films/:id' + process.argv[2], (error, response, body) => {
+  if (error) {
+    console.error('error:', error);
+  }
+  console.log(JSON.parse(body).title);
+});
