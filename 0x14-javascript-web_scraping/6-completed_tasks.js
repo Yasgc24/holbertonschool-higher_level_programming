8c1b454a0a1eb0e9b4f7cdc25a482a21021e1fe5
Yasgc24 <4537@holbertonschool.com>
2022-08-17 19:32:00 -0500

    Modified task 6 solution

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
+const { error } = require('console');
 const request = require('request');
 
-request.get(process.argv[2], (error, response, body) => {
+request(process.argv[2], (error, response, body) => {
   if (!error) {
+    console.error('error:', error);
     const completed = {};
     JSON.parse(body).forEach((tasks) => {
       if (tasks.completed && completed[tasks.userId] === undefined) {
