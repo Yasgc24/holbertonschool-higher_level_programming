0a150217ba90554533e8812b359bd1ff6f4a1986
Yasgc24 <4537@holbertonschool.com>
2022-08-17 19:48:28 -0500

    Add task 4 solution

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+const request = require('request');
+
+request(process.argv[2], (err, response, body) => {
+  if (err) {
+    console.error(err);
+  }
+  let count = 0;
+  for (const result of JSON.parse(body).results) {
+    for (const urlwed of result.characters) {
+      if (urlwed.includes(18)) {
+        count++;
+      }
+    }
+  }
+  console.log(count);
+});
