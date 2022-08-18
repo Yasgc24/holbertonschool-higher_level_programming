48a6641e648ad5300d0f8075abd7c4d29cfc41af
Yasgc24 <4537@holbertonschool.com>
2022-08-17 19:29:00 -0500

    Add task 6 solution

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+const request = require('request');
+
+request.get(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.error('error:', error);
+    const completed = {};
+    JSON.parse(body).forEach((tasks) => {
+      if (tasks.completed && completed[tasks.userId] === undefined) {
+        completed[tasks.userId] = 1;
+      } else if (tasks.completed) {
+        completed[tasks.userId] += 1;
+      }
+    });
+  }
+});
