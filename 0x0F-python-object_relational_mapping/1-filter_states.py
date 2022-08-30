eb73374ff1d1056a538635ebdf9301b3d3f165be
Yasgc24 <4537@holbertonschool.com>
2022-08-30 17:33:59 -0500

    Add task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"""
+import MySQLdb
+from sys import argv
+
+
+def get_states_n():
+    """Takes 3 arguments:
+        argv[1]: mysql username
+        argv[2]: mysql password
+        argv[3]: database name"""
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    cursor = db.cursor()
+
+    cursor.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
+    rows = cursor.fetchall()
+    for i in rows:
+        print(i)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    get_states_n()
