4e20c3382707a6762a1fa2d31276481c76a45bd9
Yasgc24 <4537@holbertonschool.com>
2022-08-30 23:38:21 -0500

    Add task 4 and modified task 3

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""script that takes in arguments and displays all values in the states table
-of hbtn_0e_0_usa where name matches the argument."""
+"""script that lists all cities from the database hbtn_0e_4_usa"""
 import MySQLdb
 from sys import argv
 
@@ -9,8 +8,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
     cursor = db.cursor()
-    cursor.execute('SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC',
-                   (argv[4],))
+    cursor.execute('SELECT cities.id, cities.name, states.name FROM cities \
+    JOIN states ON cities.state_id = states.id ORDER BY cities.id')
     rows = cursor.fetchall()
     for row in rows:
         print(row)
