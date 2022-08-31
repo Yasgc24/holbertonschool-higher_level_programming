cdbdee54a60a38d846631159d139a146a552e59d
Yasgc24 <4537@holbertonschool.com>
2022-08-31 13:27:37 -0500

    Modified task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,13 +7,12 @@ from sys import argv
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
+                         passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
-    # HERE I have to know SQL to grab all states in my database
     cur.execute('SELECT cities.name FROM cities \
     JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s \
     ORDER BY cities.id', (argv[4],))
     query_rows = cur.fetchall()
-    print(", ".join(city[0] for city in rows))
+    print(", ".join(city[0] for city in query_rows))
     cur.close()
     conn.close()
