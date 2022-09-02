40f8042e1b118a47461096727d1fcdeb7880d989
Yasgc24 <4537@holbertonschool.com>
2022-09-02 16:21:57 -0500

    Modified task 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,6 +8,6 @@ from sys import argv
 
 if __name__ == "__main__":
     url = "https://api.github.com/users/{}".format(argv[1])
-    headers = {"Authorization": "token" + argv[2]}
+    headers = {"Authorization": "token " + argv[2]}
     response = requests.get(url, headers=headers)
     print(response.json().get("id"))
